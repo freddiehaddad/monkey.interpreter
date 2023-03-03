e9133bd5f78eff8e2a0c62dd36ebeef61ba879fe
Freddie Haddad <freddie.haddad@outlook.com>
2023-03-03 12:29:01 -0800

    feat(parser): Framework for AST and parser plus initial parsing
    The initial implementation for the AST and parser support the basic data
    structures and simple let statements.  There's also a complete unit test
    for simple let statements.
    
    Subsequent commits will build on this framework adding incremental
    support for the full language.

---
+++ src/monkey/parser/parser.go
@@ -0,0 +1,110 @@
+// Monkey language parser
+package parser
+
+import (
+	"log"
+	"monkey/ast"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// Read two tokens, so curToken and peekToken are both set
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for !p.curTokenIs(token.EOF) {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
+}
+
+// Checks if the current token's type matches `t`.  Return true if the types match,
+// false otherwise.
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+// Checks if the next token to be processed (the look ahead token) type matches `t`.
+// Returns true if so, false otherwise.
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+// Checks if the next token's type matches the type of `t`.  If the type matches
+// the expected type `t`, then the token is advanced and `true` is returned.
+// If the type does match, then false is returned without token advancement.
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		return false
+	}
+}
+
+// Looks at the current token's type and attempts to process the next series of
+// tokens according to the grammar for statement definition.
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		log.Printf("Support for %q not implemented\n", p.curToken.Type)
+		return nil
+	}
+}
+
+// Implementation for the `let` statement definition.
+// The expected form is:
+//
+//	let IDENTIFIER = EXPRESSION;
+func (p *Parser) parseLetStatement() ast.Statement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.IDENT) {
+		log.Printf("Expected next token to be of type=%q, got=%q\n", token.IDENT, p.peekToken.Type)
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		log.Printf("Expected next token to be of type=%q, got=%q\n", token.ASSIGN, p.peekToken.Type)
+		return nil
+	}
+
+	// TODO: We're skipping the expressions until we encounter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		log.Printf("Intentionally skipping all tokens up to the SEMICOLON\n")
+		p.nextToken()
+	}
+
+	return stmt
+}
