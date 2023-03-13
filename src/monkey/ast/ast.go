fd6063fbfcb6d358076e1751fa4598733fa16d3e
Freddie Haddad <freddie.haddad@outlook.com>
2023-03-12 18:52:59 -0700

    feat(parser): add support for parsing integer literals
    This PR extends the Pratt parser with support for integer literals and a
    unit test confirming correct parsing behavior.

--- src/monkey/ast/ast.go
+++ src/monkey/ast/ast.go
@@ -97,6 +97,15 @@ func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 func (i *Identifier) String() string       { return i.Value }
 
+type IntegerLiteral struct {
+	Token token.Token // the token.IDENT token
+	Value int64
+}
+
+func (i *IntegerLiteral) expressionNode()      {}
+func (i *IntegerLiteral) TokenLiteral() string { return i.Token.Literal }
+func (i *IntegerLiteral) String() string       { return i.Token.Literal }
+
 type ExpressionStatement struct {
 	Token      token.Token
 	Expression Expression
