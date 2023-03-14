76f5e51135ffc79f0f305fe52af980d3a4a05e48
Freddie Haddad <6127369+freddiehaddad@users.noreply.github.com>
2023-03-14 11:52:37 -0700

    test(parser): add boolean tests to TestOperatorPrecedenceParsing (#15)

--- pkg/parser/parser_test.go
+++ pkg/parser/parser_test.go
@@ -417,6 +417,22 @@ func TestOperatorPrecendenceParsing(t *testing.T) {
 			"3 + 4 * 5 == 3 * 1 + 4 * 5",
 			"((3 + (4 * 5)) == ((3 * 1) + (4 * 5)))",
 		},
+		{
+			"true",
+			"true",
+		},
+		{
+			"false",
+			"false",
+		},
+		{
+			"3 > 5 == false",
+			"((3 > 5) == false)",
+		},
+		{
+			"3 < 5 == true",
+			"((3 < 5) == true)",
+		},
 	}
 
 	for _, tt := range tests {
