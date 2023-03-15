542278a3f5c222d763272df1de78a62fd8d2fb67
Freddie Haddad <6127369+freddiehaddad@users.noreply.github.com>
2023-03-15 14:26:08 -0700

    feat(parser): Add support for grouped expressions (#17)
    Grouped expressions are expressions surrounded by parenthesis.  For
    example, instead of:
    
        1 + 2 + 3
    
    We have:
    
        1 + (2 + 3)

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -85,6 +85,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 	p.registerPrefix(token.TRUE, p.parseBoolean)
 	p.registerPrefix(token.FALSE, p.parseBoolean)
+	p.registerPrefix(token.LPAREN, p.parseGroupedExpression)
 
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
 	p.registerInfix(token.PLUS, p.parseInfixExpression)
@@ -328,6 +329,18 @@ func (p *Parser) parsePrefixExpression() ast.Expression {
 
 	expression.Right = p.parseExpression(PREFIX)
 	return expression
+}
+
+func (p *Parser) parseGroupedExpression() ast.Expression {
+	p.nextToken()
+
+	exp := p.parseExpression(LOWEST)
+
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
+	return exp
 }
 
 // Helper function to add entries to the parser's prefix map.
