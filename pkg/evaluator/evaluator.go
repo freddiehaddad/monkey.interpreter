2a70c2cdd3006f3bcc841a9a26c90e66f0290144
Freddie Haddad <freddie.haddad@outlook.com>
2023-03-20 05:42:27 -0700

    feat(evaluator): add support for minus (-) prefix

--- pkg/evaluator/evaluator.go
+++ pkg/evaluator/evaluator.go
@@ -49,6 +49,8 @@ func evalPrefixExpression(operator string, right object.Object) object.Object {
 	switch operator {
 	case "!":
 		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusPrefixOperatorExpression(right)
 	default:
 		return NULL
 	}
@@ -65,6 +67,15 @@ func evalBangOperatorExpression(right object.Object) object.Object {
 	default:
 		return FALSE
 	}
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
 }
 
 func nativeBoolToBooleanObject(input bool) *object.Boolean {
