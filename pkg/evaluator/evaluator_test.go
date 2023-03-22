4a6a4cdd35674b92831e74891a79948cbaad8d5d
Freddie Haddad <6127369+freddiehaddad@users.noreply.github.com>
2023-03-21 18:39:36 -0700

    feat(evaluator): add if-expression parsing support (#37)
    In the language specification, truthiness has been determined to be
    anything other than false and NULL.

--- pkg/evaluator/evaluator_test.go
+++ pkg/evaluator/evaluator_test.go
@@ -9,6 +9,31 @@ import (
 	"github.com/freddiehaddad/monkey.interpreter/pkg/parser"
 )
 
+func TestIfEseExpressions(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{"if (true) { 10 }", 10},
+		{"if (false) { 10 }", nil},
+		{"if (1) { 10 }", 10},
+		{"if (1 < 2) { 10 }", 10},
+		{"if (1 > 2) { 10 }", nil},
+		{"if (1 > 2) { 10 } else { 20 }", 20},
+		{"if (1 < 2) { 10 } else { 20 }", 10},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		integer, ok := tt.expected.(int)
+		if ok {
+			testIntegerObject(t, evaluated, int64(integer))
+		} else {
+			testNullObject(t, evaluated)
+		}
+	}
+}
+
 func TestBangOperator(t *testing.T) {
 	tests := []struct {
 		input    string
@@ -107,6 +132,15 @@ func testEval(input string) object.Object {
 	program := p.ParseProgram()
 
 	return Eval(program)
+}
+
+func testNullObject(t *testing.T, obj object.Object) bool {
+	if obj != NULL {
+		t.Errorf("object is not NULL. got=%T (%+v)", obj, obj)
+		return false
+	}
+
+	return true
 }
 
 func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
