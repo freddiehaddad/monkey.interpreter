e4e6b0aa28141e8c54c0c25c26118eb75b59f1d5
Freddie Haddad <6127369+freddiehaddad@users.noreply.github.com>
2023-03-24 13:58:01 -0700

    feat(env): add a runtime environment for let statement support (#40)
    The runtime environment enables the interpreter to persist state for
    variables.  The initial implementation is a hash map of strings to
    objects that store the identifier as the key and the object as the
    value.  This will be extended when function call handling is added.
    
    As part of the testing and support for the environment, let statement
    functionality is also part of this commit.

--- pkg/repl/repl.go
+++ pkg/repl/repl.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/freddiehaddad/monkey.interpreter/pkg/evaluator"
 	"github.com/freddiehaddad/monkey.interpreter/pkg/lexer"
+	"github.com/freddiehaddad/monkey.interpreter/pkg/object"
 	"github.com/freddiehaddad/monkey.interpreter/pkg/parser"
 )
 
@@ -15,6 +16,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Fprintf(out, PROMPT)
@@ -33,7 +35,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
