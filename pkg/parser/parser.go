75fab8fa269a21b6d94a27712f50e94ec7b4bd16
Freddie Haddad <freddie.haddad@outlook.com>
2023-03-27 19:40:55 -0700

    refactor(cleanup): make parseExpressionList generic for reuse
    parseCallExpression and parseArrayLiteral now share parseExpressionList
    since they follow the same pattern.

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -489,34 +489,8 @@ func (p *Parser) parseExpressionList(end token.TokenType) []ast.Expression {
 func (p *Parser) parseCallExpression(function ast.Expression) ast.Expression {
 	// defer untrace(trace("parseCallExpression"))
 	exp := &ast.CallExpression{Token: p.curToken, Function: function}
-	exp.Arguments = p.parseCallArguments()
+	exp.Arguments = p.parseExpressionList(token.RPAREN)
 	return exp
-}
-
-func (p *Parser) parseCallArguments() []ast.Expression {
-	// defer untrace(trace("parseCallArguments"))
-	args := []ast.Expression{}
-
-	if p.peekTokenIs(token.RPAREN) {
-		p.nextToken()
-		return args
-	}
-
-	p.nextToken()
-	args = append(args, p.parseExpression(LOWEST))
-
-	for p.peekTokenIs(token.COMMA) {
-		p.nextToken()
-		p.nextToken()
-		args = append(args, p.parseExpression(LOWEST))
-
-	}
-
-	if !p.expectPeek(token.RPAREN) {
-		return nil
-	}
-
-	return args
 }
 
 func (p *Parser) parseIndexExpression(left ast.Expression) ast.Expression {
