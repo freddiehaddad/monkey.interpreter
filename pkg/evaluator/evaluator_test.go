f6979aec0170f674e73f46c6b64fea7a863f2746
Freddie Haddad <freddie.haddad@outlook.com>
2023-03-28 08:29:09 -0700

    feat(builtins): add first and last builtins for arrays
    Returns the first or last element in an array and NULL for empty arrays.

--- pkg/evaluator/evaluator_test.go
+++ pkg/evaluator/evaluator_test.go
@@ -364,6 +364,10 @@ func TestBuiltinFunctions(t *testing.T) {
 		{`len("hello world")`, 11},
 		{`len(1)`, "argument to `len` not supported, got INTEGER"},
 		{`len("one", "two")`, "wrong number of arguments. got=2, want=1"},
+		{`len([])`, 0},
+		{`len([1])`, 1},
+		{`first([1])`, 1},
+		{`last([1])`, 1},
 	}
 
 	for _, tt := range tests {
